351237423a2003331c5a3a25928c48bd912c5e4e
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-03 21:54:52 +0200

    Basic use of record batches

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Python-Packages-Tutorials
+
---
+++ pyarrow/record_batches/basic-use.py
@@ -0,0 +1,25 @@
+import pyarrow as pa
+
+
+# Creation
+arrays = [
+    pa.array([1, 2, 3, 4]),
+    pa.array(['foo', 'bar', 'baz', None]),
+    pa.array([True, None, False, True])
+    ]
+
+batch = pa.RecordBatch.from_arrays(arrays, ['f0', 'f1', 'f2'])
+
+# Simple features
+batch.schema
+
+# Projection
+batch['f1']
+batch[0]
+
+# Slice (no copy)
+batch_slice = batch.slice(1,3)
+batch_slice[1]
+
+
+
