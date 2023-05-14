ba6b1451f31335eaed083430b3cf8b4bd2997d66
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-14 21:32:49 +0200

    Generating disturbance by guassian filter (actual distrubance script)

--- plotly/2d/disturbance.py
+++ plotly/2d/disturbance.py
@@ -1,3 +1,6 @@
+from typing import List
+import math
+from scipy.ndimage import gaussian_filter
 import plotly
 import plotly.graph_objects as go
 import numpy as np
@@ -5,28 +8,24 @@ import numpy as np
 ##############################################################################
 # 1D disturbance
 ##############################################################################
-
-
-
-num_samples = 1000
-# invs
-shortterm_variance = 100
-longterm_variance = 20
-
-
-
-window_size = int(2*longterm_variance+1)
-disturbance_sample = np.random.randn(num_samples)
-
-for _ in range(shortterm_variance):
-    disturbance_sample = np.hstack([disturbance_sample[-int(window_size/2):], disturbance_sample, disturbance_sample[0: int(window_size/2)]])
-    disturbance_sample = np.convolve(disturbance_sample, np.ones(window_size)/window_size, mode='valid')
-
-disturbance = disturbance_sample
+num_samples = 10000
+smoothness = 10
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(
-    y=disturbance, # fill area between trace0 and trace1
+    y=gaussian_filter(np.random.randn(num_samples), smoothness, mode='wrap'), # fill area between trace0 and trace1
     mode='lines', line_color='indigo'))
 
 fig.show()
+
+
+
+sigmas = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+stds = []
+ms = []
+for s in sigmas:
+    stds.append(np.std(gaussian_filter(np.random.randn(num_samples), s, mode='wrap')))
+    ms.append(np.mean(gaussian_filter(np.random.randn(num_samples), s, mode='wrap')))
+
+stds
+ms
