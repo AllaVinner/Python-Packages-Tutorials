5eac3aa22539270675c260bb795a6e2b8c6ce8a2
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-15 22:33:16 +0200

    Working 2d to 3d plots with alpha

--- plotly/2d/non-grid-surface.py
+++ plotly/2d/non-grid-surface.py
@@ -20,7 +20,7 @@ from PIL import Image
 from scipy.ndimage import gaussian_filter
 
 N = 1000
-smoothness = 0.03
+smoothness = 0.04 
 
 
 theta = np.linspace(0, 2*np.pi, N)
@@ -30,7 +30,6 @@ disturbance = gaussian_filter(np.random.randn(N,1), sigma= N*smoothness, mode='w
 
 Rho, Theta = np.meshgrid(rho, theta)
 R = Rho*(5+np.cos(5*Theta+2*np.pi/12) + 10*disturbance)
-R = Rho*(5+ 10*disturbance)
 
 X = R*np.cos(Theta)
 Y = R*np.sin(Theta)
@@ -40,7 +39,8 @@ Z = np.sin(X) + np.cos(Y/10)*Y
 
 fig = make_subplots(rows=1, cols=2,
                     specs=[[{'is_3d': True}, {'is_3d': True}]],
-                    subplot_titles=['Color corresponds to z', 'Color corresponds to distance to origin'],
+                    subplot_titles=['Domain', 
+                                    'Image'],
                     )
 fig.add_trace(go.Surface(x=X, y=Y, z=np.zeros(X.shape), surfacecolor=X+Y), 1, 1)
 fig.add_trace(go.Surface(x=X, y=Y, z=Z, surfacecolor=X+Y), 1, 2)
@@ -48,8 +48,8 @@ fig.add_trace(go.Surface(x=X, y=Y, z=Z, surfacecolor=X+Y), 1, 2)
 axis_range = 10
 fig.update_layout(
     scene = dict(
-        xaxis = dict(visible=False),
-        yaxis = dict(visible=False),
+        xaxis = dict(visible=False, range=[-axis_range, axis_range]),
+        yaxis = dict(visible=False, range=[-axis_range, axis_range]),
         zaxis =dict(visible=True,showticklabels=False, title='')
     ),
     scene2 = dict(
@@ -74,6 +74,9 @@ camera2 = dict(
 
 fig.update_layout(scene=dict(camera=camera),
                   scene2=dict(camera=camera2))
+fig.update_traces(showscale=False)
+fig.update_traces(colorscale=[[0.0, "rgba(26,150,64, 0.7)"], [1, "rgba(26,150,65, 0.7)"]], row=1, col=1)
+fig.update_traces(colorscale=a, row=1, col=2)
 fig.show()
 
 write_fig_tree(fig, 'sample.json')
