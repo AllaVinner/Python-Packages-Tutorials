72aa37ca11dc844818515e4e6f4344fab97f1f5e
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-18 19:20:29 +0200

    First NCBI Call

--- biopython/scratch.py
+++ biopython/scratch.py
@@ -1,5 +1,19 @@
 import Bio
 
+# Read a fasta file and align to 
+
+
+from Bio.Blast import NCBIWWW
+
+
+fasta_string =  open("./biopython/data/myseq.fa").read()
+
+# Call over internet
+result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
+
+
+
+
 
 
 
