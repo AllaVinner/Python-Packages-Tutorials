0186eec5486197164d2d884fff41184807d7d633
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-23 19:44:01 +0200

    Adding parsing and indexing methods to jbio module

--- biopython/DNA-Algorithms/jbio/pattern_matching.py
+++ biopython/DNA-Algorithms/jbio/pattern_matching.py
@@ -12,6 +12,51 @@ def naive_exact_matching(p, t):
             occurrences.append(i)  # all chars matched; record
     return occurrences
 
+
+def naive_haming_match(p, t, max_error = 2):
+    occurrences = []
+    for i in range(len(t) - len(p) + 1):  # loop over alignments
+        match = True
+        num_err = 0
+        for j in range(len(p)):  # loop over characters
+            if t[i+j] != p[j]:  # compare characters
+                num_err += 1
+                if num_err > max_error:
+                    match = False
+                    break
+        if match:
+            occurrences.append(i)  # all chars matched; record
+    return occurrences
+
+
+def naive_leniant_match(p, t, distance_fun, max_error = 2):
+    occurrences = []
+    for i in range(len(t) - len(p) + 1):  # loop over alignments
+        if distance_fun(t[i:i+len(p)], p) <= max_error: 
+            occurrences.append(i)
+    return occurrences
+
+def naive_leniant_best_match(p, t, distance_fun, keep: int = 1):
+    best_matches = []
+    best_scores = []
+    for i in range(min(keep, len(t) - len(p) + 1)):
+        best_matches.append(t[i:i+len(p)])
+        best_scores.append(distance_fun(t[i:i+len(p)], p))
+        
+    for i in range(keep, len(t) - len(p) + 1):  # loop over alignments
+        min_val = min(best_scores)
+        score = distance_fun(t[i:i+len(p)], p)
+        if score < min_val:
+            min_index = best_scores.index(min_val)
+            best_scores.pop(min_index)
+            best_matches.pop(min_index)
+            best_matches.append(i)
+            best_scores.append(score)
+
+    return best_matches, best_scores
+
+
+
 def boyer_more(p, p_bm, t):
     i = 0
     occurerences = list()
@@ -35,8 +80,6 @@ def boyer_more(p, p_bm, t):
             shift = max(shift, skip_gs)
         i += shift
     return occurerences, char_comparisons, alignments
-
-
 
 
 
