6638f14a130dede63440d70c4a75d464cfc074c1
AllaVinner <joel.w.ottosson@gmail.com>
2023-06-28 17:35:31 +0200

    Added list type example

--- pyarrow/compond_datatypes/array.py
+++ pyarrow/compond_datatypes/array.py
@@ -9,11 +9,8 @@ import pyarrow.parquet as pq
 st = pa.struct([pa.field('x', pa.float32()), pa.field('y', pa.float32())])
 days = pa.array([{'x': 1., 'y': 2.3}, {'x': 2., 'y': 4.}], type=st)
 
-
-
-
-
-
+lt = pa.list_(pa.float32())
+years = pa.array([[1.,  2.3], [ 2., 4.]], type=lt)
 
 
 
