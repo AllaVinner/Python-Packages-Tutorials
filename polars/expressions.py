75236e048edc0734d21bb43a924b34f222dee0f2
AllaVinner <joel.w.ottosson@gmail.com>
2023-07-05 21:17:36 +0200

    Added iris dataset and update the expressions to align

--- polars/expressions.py
+++ polars/expressions.py
@@ -8,17 +8,17 @@ pl.col("foo").sort().head(2)
 # Lazy vs Eager API
 
 # Eager
-df = pl.read_csv("docs/src/data/iris.csv")
-df_small = df.filter(pl.col("sepal_length") > 5)
-df_agg = df_small.groupby("species").agg(pl.col("sepal_width").mean())
+df = pl.read_csv("polars/data/iris.csv")
+df_small = df.filter(pl.col("sepal.length") > 5)
+df_agg = df_small.groupby("variety").agg(pl.col("sepal.width").mean())
 print(df_agg)
 
 # Lazy
 q = (
-    pl.scan_csv("docs/src/data/iris.csv")
-    .filter(pl.col("sepal_length") > 5)
-    .groupby("species")
-    .agg(pl.col("sepal_width").mean())
+    pl.scan_csv("polars/data/iris.csv")
+    .filter(pl.col("sepal.length") > 5)
+    .groupby("variety")
+    .agg(pl.col("sepal.width").mean())
 )
 
 df = q.collect()
@@ -26,10 +26,10 @@ df = q.collect()
 # Lazy Streamable
 
 q = (
-    pl.scan_csv("docs/src/data/iris.csv")
-    .filter(pl.col("sepal_length") > 5)
-    .groupby("species")
-    .agg(pl.col("sepal_width").mean())
+    pl.scan_csv("polars/data/iris.csv")
+    .filter(pl.col("sepal.length") > 5)
+    .groupby("variety")
+    .agg(pl.col("sepal.width").mean())
 )
 
 df = q.collect(streaming=True)
