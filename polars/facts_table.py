8f5d2eab05491b23d6b58295d95791d834bdd472
AllaVinner <joel.w.ottosson@gmail.com>
2023-07-07 23:57:49 +0200

    Polars: Initiating join of var table

--- polars/facts_table.py
+++ polars/facts_table.py
@@ -2,7 +2,8 @@ import polars as pl
 import numpy as np
 import string
 import random
-
+from dataclasses import dataclass
+from enum import Enum, auto
 
 # Setup data
 def get_ids(num_ids, len_ids = 5):
@@ -11,6 +12,10 @@ def get_ids(num_ids, len_ids = 5):
 
 def get_categorical(num_samples, categories):
     return [random.choice(categories) for _ in range(num_samples)]
+
+
+def get_unique_option(num_samples, options):
+    return np.random.choice(options,(num_samples, ), replace = False).tolist()
 
 
 def get_int(num_samples, min = 0, max = 10):
@@ -28,47 +33,110 @@ def get_facts_table(num_row, num_col, density = 0.3):
     vals = [random.random() for _ in range(num_expr)]
     return rows, cols, vals
 
+global_ids = get_ids(20, len_ids=5)
 
+##############################################################################
+# Expression 1
+##############################################################################
 # Obs table
-obs_len = 10
-obs = pl.DataFrame(
+num_rows = 10
+obs1 = pl.DataFrame(
     dict(
-        id=get_ids(obs_len, len_ids=5),
-        cancer_type=get_categorical(obs_len, ['Carcinoma', 'Lung Cancer', 'Prostate Cancer', 'Leukemia']),
-        progression=get_int(obs_len, 0, 10),
-        obs_id=[i for i in range(obs_len)]
+        id=get_ids(num_rows, len_ids=10),
+        cancer_type=get_categorical(num_rows, ['Carcinoma', 'Lung Cancer', 'Prostate Cancer', 'Leukemia']),
+        progression=get_int(num_rows, 0, 10),
+        obs_id=[i for i in range(num_rows)]
     )
 )
-print(obs)
+print(obs1)
 
 # var tabl
-var_len = 5
-var = pl.DataFrame(
+num_rows = 5
+var1 = pl.DataFrame(
     dict(
-        id=get_ids(var_len, len_ids=10),
-        region=get_categorical(var_len, ['Europe', 'Asia', 'Africa', 'America']),
-        promoter=get_boolean(var_len),
-        var_id=[i for i in range(var_len)]
+        id=get_unique_option(num_rows, global_ids),
+        region=get_categorical(num_rows, ['Europe', 'Asia', 'Africa', 'America']),
+        promoter=get_boolean(num_rows),
+        var_id=[i for i in range(num_rows)]
     )
 )
-print(var)
-
+print(var1)
 
 # X table
-rows, cols, vals = get_facts_table(obs_len, var_len, density=0.3)
-x = pl.DataFrame(
+rows, cols, vals = get_facts_table(len(obs1), len(var1), density=0.3)
+x1 = pl.DataFrame(
     dict(
         obs_id=rows,
         var_id=cols,
         val=vals
     )
 )
-print(x)
 
-# Create sparse matrix
-import scipy.sparse
+##############################################################################
+# Expression 2
+##############################################################################
+# Obs table
+num_rows = 2
+obs2 = pl.DataFrame(
+    dict(
+        id=get_ids(num_rows, len_ids=10),
+        cancer_type=get_categorical(num_rows, ['Carcinoma', 'Lung Cancer', 'Prostate Cancer', 'Leukemia']),
+        progression=get_int(num_rows, 0, 10),
+        obs_id=[i for i in range(num_rows)]
+    )
+)
+
+# var tabl
+num_rows = 10
+var2 = pl.DataFrame(
+    dict(
+        id=get_unique_option(num_rows, global_ids),
+        region=get_categorical(num_rows, ['Europe', 'Asia', 'Africa', 'America']),
+        promoter=get_boolean(num_rows),
+        var_id=[i for i in range(num_rows)]
+    )
+)
+
+# X table
+rows, cols, vals = get_facts_table(len(obs2), len(var2), density=0.3)
+x2 = pl.DataFrame(
+    dict(
+        obs_id=rows,
+        var_id=cols,
+        val=vals
+    )
+)
 
 
-m = scipy.sparse.coo_array((vals,(rows, cols)))
+# Join tables
+var1_unique = [col for col in var1.columns if col not in var2.columns]
+var2_unique = [col for col in var2.columns if col not in var1.columns]
+duplicat_columns = [col for col in var1.columns if col in var2.columns and col != 'id']
+
+suffix = '_right'
+(
+    var1
+    .join(var2, on='id', how='outer', suffix=suffix)
+    .with_columns(pl.coalesce(["region", "region_right"]).alias("region"))
+    .with_columns(pl.coalesce(["region", "region_right"]).alias("region"))
+
+)
+
+join_table = var1.join(var2, on='id', how='outer', suffix=suffix).sort(by='var_id')
+
+for dub_col in duplicat_columns:
+    join_table = join_table.with_columns(pl.coalesce([dub_col, dub_col+suffix]).alias(dub_col)).drop(dub_col+suffix)
+
+join_table.with_columns([
+        pl.col("var_id")
+    ]
+)
+
+var1_unique
+var2_unique
+duplicat_columns
+
+
+
 
 
